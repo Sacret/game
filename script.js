8bc8fa5a20868cdda69fc211734db5e31f57826f
Abakumova Anastasiya <a.abakumova@rambler-co.ru>
2023-02-22 21:03:31 +0300

    Add on/off, pause, reset butons and splash screen

--- script.js
+++ script.js
@@ -1,16 +1,56 @@
 class Game {
   static #ROW_LENGTH = 20;
   static #COLUMN_LENGTH = 10;
+  static #START_POSITION_X = 3;
+  static #CHANGE_DELAY_INTERVAl = 10;
+
+  static #FIGURES = {
+    // I
+    0: [
+      [false, false, false, false],
+      [true, true, true, true]
+    ],
+    // J
+    1: [
+      [true, false, false],
+      [true, true, true]
+    ],
+    // L
+    2: [
+      [false, false, true],
+      [true, true, true]
+    ],
+    // O
+    3: [
+      [true, true],
+      [true, true]
+    ],
+    // S
+    4: [
+      [false, true, true],
+      [true, true, false]
+    ],
+    // T
+    5: [
+      [false, true, false],
+      [true, true, true]
+    ],
+    // Z
+    6: [
+      [true, true, false],
+      [false, true, true]
+    ],
+  };
   static #FIGURES_COUNT = 7;
 
-  static #START_POSITION = 3;
-
-  static #START_INTERVAL = 1000;
-  static #DIFF_INTERVAL = 50;
-  static #SPEED_INTERVAL = 100;
-  static #MIN_INTERVAl = 0;
+  static #START_DELAY = 1000;
+  static #DIFF_DELAY = 50;
+  static #SPEED_DELAY = 100;
+  static #MIN_DELAY = 0;
+  static #SPLASH_DELAY = 20;
 
   static #CELL = '▣';
+  static #HI_SCORE_LABEL = 'hi-score';
 
   static #SCORES_LEVEL = {
     0: 0,
@@ -21,86 +61,153 @@ class Game {
   };
 
   static #EMPTY_FIELD =  [...Array(Game.#ROW_LENGTH)].map(
-    row => [...Array(Game.#COLUMN_LENGTH)].map(column => false)
+    row => [...Array(Game.#COLUMN_LENGTH)].map(column => false),
   );
 
-  static #FIGURES = {
-    0: [
-      [false, false, false, false],
-      [true, true, true, true]
-    ], // I
-    1: [
-      [true, false, false],
-      [true, true, true]
-    ], // J
-    2: [
-      [false, false, true],
-      [true, true, true]
-    ], // L
-    3: [
-      [true, true],
-      [true, true]
-    ], // O
-    4: [
-      [false, true, true],
-      [true, true, false]
-    ], // S
-    5: [
-      [false, true, false],
-      [true, true, true]
-    ], // T
-    6: [
-      [true, true, false],
-      [false, true, true]
-    ], // Z
-  };
+  static #SPLASH_SCREEN = [
+    [true, true, true, false, true, true, true, false, false, false],
+    [false, true, false, false, true, false, false, false, false, false],
+    [false, true, false, false, true, true, true, false, false, false],
+    [false, true, false, false, true, false, false, false, false, false],
+    [false, true, false, false, true, true, true, false, false, false],
+    [false, false, false, false, false, false, false, false, false, false],
+    [false, false, false, true, true, true, false, true, true, true],
+    [false, false, false, false, true, false, false, true, false, true],
+    [false, false, false, false, true, false, false, true, true, true],
+    [false, false, false, false, true, false, false, true, true, false],
+    [false, false, false, false, true, false, false, true, false, true],
+    [false, false, false, false, false, false, false, false, false, false],
+    [true, true, true, false, true, true, true, false, false, false],
+    [false, true, false, false, true, false, false, false, false, false],
+    [false, true, false, false, true, true, true, false, false, false],
+    [false, true, false, false, false, false, true, false, false, false],
+    [true, true, true, false, true, true, true, false, false, false],
+    [false, false, false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false, false, false],
+  ];
 
   #scores = 0;
   #figuresCount = 0;
-  #currentTimeout = 0;
-  #interval = Game.#START_INTERVAL;
-  #currentInterval = this.#interval;
   #step = 0;
+  #currentPositionX = Game.#START_POSITION_X;
+
+  #currentTimeoutId = 0;
+  #delay = Game.#START_DELAY;
+  #currentDelay = this.#delay;
+
   #field = Game.#EMPTY_FIELD;
-  #currentPosition = Game.#START_POSITION;
   #figure = null;
   #figureField = Game.#EMPTY_FIELD;
 
-  constructor(gameElem, scoresElem, hiScoresElem) {
+  #isGameInProgress = false;
+  #isGameOn = false;
+
+  constructor(gameElem, gameInfoElem, scoresElem, hiScoresElem) {
     this.gameElem = gameElem;
+    this.gameInfoElem = gameInfoElem;
     this.scoresElem = scoresElem;
     this.hiScoresElem = hiScoresElem;
 
     this.#setHiScore();
   }
 
-  #clearCurrentTimeout = () => {
-    clearTimeout(this.#currentTimeout);
+  #setHiScore = () => {
+    hiScoresElem.innerText = localStorage.getItem(Game.#HI_SCORE_LABEL) || 0;
   }
 
-  #setHiScore = () => {
-    hiScoresElem.innerText = localStorage.getItem('hi-score') || 0;
+  #clearCurrentTimeout = () => {
+    clearTimeout(this.#currentTimeoutId);
+  }
+
+  #changeDelay = () => {
+    if (this.#figuresCount % Game.#CHANGE_DELAY_INTERVAl === 0) {
+      this.#delay = this.#delay - Game.#DIFF_DELAY > Game.#SPEED_DELAY
+        ? this.#delay - Game.#DIFF_DELAY
+        : Game.#SPEED_DELAY;
+    }
+  };
+
+  #getRandomFigure = () => {
+    let rand = Math.random() * Game.#FIGURES_COUNT - 0.5;
+    return Game.#FIGURES[Math.round(rand)];
+  };
+
+  #getCell = (val) => `<span class="cell ${val ? 'filled' : 'empty'}">${Game.#CELL}</span>`;
+
+  #printSplash = (callback) => {
+    let splashStep = 0;
+
+    const printCallback = () => {
+      let strField = '';
+
+      for (let i = 0; i < Game.#ROW_LENGTH; i++) {
+        for (let j = 0; j < Game.#COLUMN_LENGTH; j++) {
+          let splashCell = false;
+          // if (splashStep < 19) {
+          //   if (19 - splashStep >= i && j === 9) {
+          //     splashCell = true;
+          //   }
+          // } else if (splashStep < 29) {
+          //   if (9 - splashStep - 20 <= j && i === 0) {
+          //     splashCell = true;
+          //   }
+          // }
+          strField += this.#getCell(Game.#SPLASH_SCREEN[i][j] || splashCell);
+        }
+        strField += '\n';
+      }
+
+      this.gameElem.innerHTML = strField;
+    }
+
+    const intervalId = setInterval(() => {
+      printCallback();
+      splashStep++;
+
+      if (!this.#isGameOn) {
+        clearInterval(intervalId);
+      }
+
+      if (splashStep === Game.#ROW_LENGTH * Game.#COLUMN_LENGTH * 2) {
+        clearInterval(intervalId);
+        callback();
+      }
+    }, Game.#SPLASH_DELAY);
   }
 
   #printField = () => {
     const field = this.#field;
     const figureField = this.#figureField;
+
     let strField = '';
 
     for (let i = 0; i < Game.#ROW_LENGTH; i++) {
       for (let j = 0; j < Game.#COLUMN_LENGTH; j++) {
-        strField += (field[i][j] || figureField[i][j])
-          ? `<span class="cell filled">${Game.#CELL}</span>`
-          : `<span class="cell empty">${Game.#CELL}</span>`;
+        strField += this.#getCell(field[i][j] || figureField[i][j]);
       }
       strField += '\n';
     }
+
     this.gameElem.innerHTML = strField;
+  };
+
+  #checkEndGame = () => {
+    let rowFilledCount = 0;
+    const field = this.#field;
+
+    for (let i = 0; i < Game.#ROW_LENGTH; i++) {
+      if (field[i].some(elem => elem)) {
+        rowFilledCount++;
+      }
+    }
+
+    return rowFilledCount === Game.#ROW_LENGTH || rowFilledCount === 0;
   };
 
   #mergeFields = () => {
     let rowCount = 0;
-    let rowFilledCount = 0;
+
     const newField = [];
     const field = this.#field;
     const figureField = this.#figureField;
@@ -114,9 +221,6 @@ class Game {
         newField[rowCount] = row;
         rowCount++;
       }
-      if (row.some(elem => elem)) {
-        rowFilledCount++;
-      }
     }
 
     const rowDiff = field.length - newField.length;
@@ -129,38 +233,8 @@ class Game {
       this.#scores += Game.#SCORES_LEVEL[rowDiff];
     }
 
-    const scores = this.#scores;
-
-    if (rowFilledCount === Game.#ROW_LENGTH) {
-      this.scoresElem.innerText = `You lost, ${scores}`;
-      localStorage.setItem('hi-score', scores);
-      this.#clearCurrentTimeout();
-    } else if (rowFilledCount === 0) {
-      this.scoresElem.innerText = `You win, ${scores}`;
-      localStorage.setItem('hi-score', scores);
-      this.#clearCurrentTimeout();
-    } else {
-      this.scoresElem.innerText = scores;
-    }
-
-    if (this.#currentInterval === Game.#MIN_INTERVAl) {
-      this.#currentInterval = this.#interval;
-    }
-
     this.#field = [...newItems, ...newField];
   }
-
-  #getRandomFigure = () => {
-    if (this.#figuresCount % 10 === 0) {
-      this.#interval = this.#interval - Game.#DIFF_INTERVAL > Game.#SPEED_INTERVAL
-        ? this.#interval - Game.#DIFF_INTERVAL
-        : Game.#SPEED_INTERVAL;
-    }
-    this.#figuresCount++;
-
-    let rand = Math.random() * Game.#FIGURES_COUNT - 0.5;
-    return Game.#FIGURES[Math.round(rand)];
-  };
 
   #getMaxPosition = (currentFigure) => {
     let maxPosition = 0;
@@ -176,36 +250,6 @@ class Game {
     return maxPosition;
   };
 
-  rotateFigure = () => {
-    const newFigure = [];
-    const figureRows = this.#figure.length;
-    const figureColumns = this.#figure[0].length;
-
-    for (let i = 0; i < figureRows; i++) {
-      for (let j = 0; j < figureColumns; j++) {
-        if (!newFigure[j]) {
-          newFigure[j] = [];
-        }
-
-        newFigure[j][figureRows - 1 - i] = this.#figure[i][j];
-      }
-    }
-
-    const nextStep = this.#step - (figureRows - newFigure.length);
-    this.#step = nextStep < Game.#ROW_LENGTH ? nextStep : Game.#ROW_LENGTH;
-
-    const nextPosition = this.#currentPosition - (figureColumns - newFigure[0].length);
-    const maxPosition = Game.#COLUMN_LENGTH - this.#getMaxPosition(newFigure);
-    if (nextPosition >= maxPosition) {
-      this.#currentPosition = maxPosition;
-    } else if (nextPosition <= 0) {
-      this.#currentPosition = 0;
-    }
-
-    this.#figure = newFigure;
-    this.#setFigureField(this.#step - 1);
-  };
-
   #setFigureField = (currentStep) => {
     this.#figureField = [...Array(Game.#ROW_LENGTH)].map(
       (_, row) => [...Array(Game.#COLUMN_LENGTH)].map((_, column) => {
@@ -214,7 +258,7 @@ class Game {
         const figureColumns = this.#figure[0].length;
 
         if (rowDiff < figureRows) {
-          const columnDiff = column - this.#currentPosition;
+          const columnDiff = column - this.#currentPositionX;
           const figureRow = this.#figure[figureRows - 1 - rowDiff];
 
           return (columnDiff >= 0 && columnDiff < figureColumns && figureRow)
@@ -242,7 +286,7 @@ class Game {
 
         const fieldRow = this.#field[this.#step - figureRows + 1 + i];
 
-        if (fieldRow && fieldRow[this.#currentPosition + j]) {
+        if (fieldRow && fieldRow[this.#currentPositionX + j]) {
           return true;
         }
       }
@@ -262,7 +306,7 @@ class Game {
 
         const fieldRow = this.#field[this.#step - figureRows + i];
 
-        if (fieldRow && fieldRow[this.#currentPosition + j - 1]) {
+        if (fieldRow && fieldRow[this.#currentPositionX + j - 1]) {
           return true;
         }
 
@@ -284,7 +328,7 @@ class Game {
 
         const fieldRow = this.#field[this.#step - figureRows + i];
 
-        if (fieldRow && fieldRow[this.#currentPosition + j + 1]) {
+        if (fieldRow && fieldRow[this.#currentPositionX + j + 1]) {
           return true;
         }
 
@@ -296,78 +340,192 @@ class Game {
   };
 
   moveFigureLeft = () => {
+    if (!this.#isGameInProgress) return;
+
     const minPosition = 0;
     if (!this.#isFieldLeftTouch()) {
-      this.#currentPosition = this.#currentPosition <= minPosition
+      this.#currentPositionX = this.#currentPositionX <= minPosition
         ? minPosition
-        : this.#currentPosition - 1;
+        : this.#currentPositionX - 1;
     }
     this.#setFigureField(this.#step - 1);
   };
 
   moveFigureRight = () => {
+    if (!this.#isGameInProgress) return;
+
     const maxPosition = Game.#COLUMN_LENGTH - this.#getMaxPosition(this.#figure);
     if (!this.#isFieldRightTouch()) {
-      this.#currentPosition = this.#currentPosition >= maxPosition
+      this.#currentPositionX = this.#currentPositionX >= maxPosition
         ? maxPosition
-        : this.#currentPosition + 1;
+        : this.#currentPositionX + 1;
     }
     this.#setFigureField(this.#step - 1);
   };
 
   moveFigureDown = () => {
-    this.#currentInterval = Game.#SPEED_INTERVAL;
+    if (!this.#isGameInProgress) return;
+
+    this.#currentDelay = Game.#SPEED_DELAY;
     this.#clearCurrentTimeout();
-    this.continueGame();
+    this.continue();
   };
 
   stopMovingFigureDown = () => {
-    this.#currentInterval = this.#interval;
+    if (!this.#isGameInProgress) return;
+
+    this.#currentDelay = this.#delay;
   };
 
   moveFigureDownQuickly = () => {
-    this.#currentInterval = Game.#MIN_INTERVAl;
+    if (!this.#isGameInProgress) return;
+
+    this.#currentDelay = Game.#MIN_DELAY;
     this.#clearCurrentTimeout();
-    this.continueGame();
+    this.continue();
   };
 
-  continueGame = () => {
+  rotateFigure = () => {
+    if (!this.#isGameInProgress) return;
+
+    const newFigure = [];
+    const figureRows = this.#figure.length;
+    const figureColumns = this.#figure[0].length;
+
+    for (let i = 0; i < figureRows; i++) {
+      for (let j = 0; j < figureColumns; j++) {
+        if (!newFigure[j]) {
+          newFigure[j] = [];
+        }
+
+        newFigure[j][figureRows - 1 - i] = this.#figure[i][j];
+      }
+    }
+
+    const nextStep = this.#step - (figureRows - newFigure.length);
+    this.#step = nextStep < Game.#ROW_LENGTH ? nextStep : Game.#ROW_LENGTH;
+
+    const nextPosition = this.#currentPositionX - (figureColumns - newFigure[0].length);
+    const maxPosition = Game.#COLUMN_LENGTH - this.#getMaxPosition(newFigure);
+    if (nextPosition >= maxPosition) {
+      this.#currentPositionX = maxPosition;
+    } else if (nextPosition <= 0) {
+      this.#currentPositionX = 0;
+    }
+
+    this.#figure = newFigure;
+    this.#setFigureField(this.#step - 1);
+  };
+
+  continue = () => {
+    if (!this.#isGameInProgress) return;
+
+    let isGameEnd = false;
+
     if (this.#step === 0) {
       this.#figure = this.#getRandomFigure();
+      this.#figuresCount++;
       this.#figureField = Game.#EMPTY_FIELD;
+      this.#changeDelay();
     }
 
     if (this.#isFieldBottomTouch()) {
       this.#mergeFields();
-      this.#step = 0;
-      this.#currentPosition = Game.#START_POSITION;
+      isGameEnd = this.#checkEndGame();
+
+      if (isGameEnd) {
+        if (this.#scores > localStorage.getItem(Game.#HI_SCORE_LABEL)) {
+          localStorage.setItem(Game.#HI_SCORE_LABEL, this.#scores);
+        }
+        this.#clearCurrentTimeout();
+      } else {
+        this.scoresElem.innerText = this.#scores;
+
+        if (this.#currentDelay === Game.#MIN_DELAY) {
+          this.#currentDelay = this.#delay;
+        }
+
+        this.#step = 0;
+        this.#currentPositionX = Game.#START_POSITION_X;
+      }
     } else {
       this.#setFigureField(this.#step);
       this.#step++;
     }
 
-    this.#currentTimeout = setTimeout(this.continueGame, this.#currentInterval);
+    if (!isGameEnd) {
+      this.#currentTimeoutId = setTimeout(this.continue, this.#currentDelay);
+    }
   };
 
-  start = () => this.continueGame();
+  start = () => {
+    this.continue();
+  }
+
+  reset = () => {
+    this.#clearCurrentTimeout();
+
+    this.#scores = 0;
+    this.#figuresCount = 0;
+    this.#step = 0;
+    this.#currentPositionX = Game.#START_POSITION_X;
+
+    this.#currentTimeoutId = 0;
+    this.#delay = Game.#START_DELAY;
+    this.#currentDelay = this.#delay;
+
+    this.#field = Game.#EMPTY_FIELD;
+    this.#figure = null;
+    this.#figureField = Game.#EMPTY_FIELD;
+
+    this.scoresElem.innerText = this.#scores;
+
+    this.#isGameInProgress = true;
+
+    this.start();
+  }
+
+  togglePause = () => {
+    this.#isGameInProgress = !this.#isGameInProgress;
+
+    if (this.#isGameInProgress) {
+      this.continue();
+    }
+  }
+
+  toggleOn = () => {
+    this.gameElem.classList.toggle('invisible');
+    this.gameInfoElem.classList.toggle('invisible');
+
+    this.#isGameOn = !this.#isGameOn;
+
+    if (this.#isGameOn) {
+      this.#printSplash(() => {
+        this.#isGameInProgress = true;
+        this.reset();
+      });
+    } else {
+      this.#isGameInProgress = false;
+    }
+  }
 };
 
 const gameElem = document.getElementById('game');
+const gameInfoElem = document.getElementById('game-info');
 const scoresElem = document.getElementById('score');
 const hiScoresElem = document.getElementById('hi-score');
 
-const game = new Game(gameElem, scoresElem, hiScoresElem);
-game.start();
+const game = new Game(gameElem, gameInfoElem, scoresElem, hiScoresElem);
 
-const KEY_LEFT_ARROW = 37;
-const KEY_UP_ARROW = 38;
-const KEY_RIGHT_ARROW = 39;
-const KEY_DOWN_ARROW = 40;
-const KEY_SPACE = 32;
+const KEY_LEFT_ARROW = 'ArrowLeft';
+const KEY_UP_ARROW = 'ArrowUp';
+const KEY_RIGHT_ARROW = 'ArrowRight';
+const KEY_DOWN_ARROW = 'ArrowDown';
+const KEY_SPACE = 'Space';
 
 document.addEventListener('keydown', (e) => {
-  const event = e = e || window.event;
-  const code = event.keyCode;
+  const event = e || window.event;
+  const code = event.code;
 
   if (code === KEY_LEFT_ARROW) {
     game.moveFigureLeft();
@@ -383,17 +541,21 @@ document.addEventListener('keydown', (e) => {
 });
 
 document.addEventListener('keyup', (e) => {
-  const event = e = e || window.event;
-  const code = event.keyCode;
+  const event = e || window.event;
+  const code = event.code;
 
   if (code === KEY_DOWN_ARROW) {
     game.stopMovingFigureDown();
   }
 });
 
-document.getElementById('button-left').addEventListener('click',  game.moveFigureLeft);
-document.getElementById('button-right').addEventListener('click',  game.moveFigureRight);
-document.getElementById('button-up').addEventListener('click',  game.moveFigureDownQuickly);
-document.getElementById('button-down').addEventListener('onmousedown',  game.moveFigureDown);
-document.getElementById('button-down').addEventListener('onmouseup',  game.stopMovingFigureDown);
-document.getElementById('button-rotate').addEventListener('click',  game.rotateFigure);
+document.getElementById('button-game-on').addEventListener('click', game.toggleOn);
+document.getElementById('button-game-pause').addEventListener('click', game.togglePause);
+document.getElementById('button-game-reset').addEventListener('click', game.reset);
+
+document.getElementById('button-left').addEventListener('click', game.moveFigureLeft);
+document.getElementById('button-right').addEventListener('click', game.moveFigureRight);
+document.getElementById('button-up').addEventListener('click', game.moveFigureDownQuickly);
+document.getElementById('button-down').addEventListener('onmousedown', game.moveFigureDown);
+document.getElementById('button-down').addEventListener('onmouseup', game.stopMovingFigureDown);
+document.getElementById('button-rotate').addEventListener('click', game.rotateFigure);
